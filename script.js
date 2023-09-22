00fd7ae63e19934fb68b587767e052f32f87c414
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-22 11:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let para = document.getElementById("timer");
+
+setInterval(()=>{
+	para.innerText = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()},  ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+},1000)
